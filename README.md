1a7ec4c19b6aaa180bce881b40786fe1d164ff22
Petr <petr_lutik@mail.ru>
2018-11-25 00:17:47 +0400

    Первая фиксация. Реализован каркас.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# realty
-otus homework 16
+﻿# Otus homework 16: realty
+[ ![Build](https://travis-ci.org/PetrLjutik/realty.svg?branch=master) ](https://travis-ci.org/PetrLjutik/realty)
+[ ![Download](https://api.bintray.com/packages/petrljutik/realty/realty/images/download.svg) ](https://bintray.com/petrljutik/realty/realty/#files)
---
+++ rclst/rclst.cpp
@@ -0,0 +1,18 @@
+﻿#include "version.h"
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  std::cout << "rclss version: "
+            << PROJECT_VERSION_MAJOR << "."
+            << PROJECT_VERSION_MINOR << "."
+            << PROJECT_VERSION_PATCH << std::endl;
+
+  if(argc != 3) {
+    std::cerr << "Wrong number of arguments.\n" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return 0;
+}
